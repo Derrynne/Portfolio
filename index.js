8b32655495bf70aa1f3bf1c7304977ff5a7182f0
DustOfDreams <52716391+Dustofdreams@users.noreply.github.com>
2023-01-26 22:30:14 +0200

    Various fixes

--- index.js
+++ index.js
@@ -5,18 +5,3 @@ function openMenu() {
 function closeMenu() {
     document.body.classList.remove('menu--open')
 }
-
-
-const scaleFactor = 1 / 20;
-
-function moveBackground(event) {
-    const shapes = document.querySelectorAll(".shape")
-    const x = event.clientX * scaleFactor;
-    const y = event.clientY * scaleFactor;
-
-    for (let i = 0; i < shapes.length; i++) {
-        const isOdd = i % 2 !== 0;
-        const oddInt = isOdd ? -1 : 1;
-        shapes[i].style.transform = `translate(${x * oddInt}px, ${y * oddInt}px)`
-    }
-}
