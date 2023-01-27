bc2468a383a78df0a7a19a222fa7ae0e65bc476b
DustOfDreams <52716391+Dustofdreams@users.noreply.github.com>
2023-01-27 14:52:58 +0200

    Various changes re width and menus

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ function openMenu() {
 function closeMenu() {
     document.body.classList.remove('menu--open')
 }
+
+const goTopBtn = document.querySelector('.go-top-btn');
+
+window.addEventListener('scroll', checkHeight)
+
+function checkHeight() {
+    if (window.scrollY > 400) {
+        goTopBtn.style.display = "flex"
+    } else {
+        goTopBtn.style.display = "none"
+    }
+}
+
+goTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+})
